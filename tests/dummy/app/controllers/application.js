3676a40df502abb0cccf1759f019bad765f2a147
Nick Schot <nickschot@gmail.com>
2019-10-24 10:52:03 +0200

    initial ecsy implementation

---
+++ tests/dummy/app/controllers/application.js
@@ -0,0 +1,10 @@
+import Controller from '@ember/controller';
+import { Object3D } from 'ember-babylon/ecsy/components/object-3d';
+
+export default class Application extends Controller {
+  init() {
+    super.init(...arguments);
+
+    this.components = [Object3D];
+  }
+}
