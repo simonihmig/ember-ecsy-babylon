d236c395a1a11648adad1d4cd74b308219a535cc
simonihmig <simon.ihmig@kaliber5.de>
2019-10-29 12:24:09 +0100

    Modifier AST transform

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 'use strict';
 
+const EcsyModiferTransform = require('./lib/ast-transform');
+
 module.exports = {
-  name: require('./package').name
+  name: require('./package').name,
+
+  setupPreprocessorRegistry(type, registry) {
+    let plugin = this._buildPlugin();
+    plugin.parallelBabel = {
+      requireFile: __filename,
+      buildUsing: '_buildPlugin',
+      params: {}
+    };
+    registry.add('htmlbars-ast-plugin', plugin);
+  },
+
+  _buildPlugin() {
+    return {
+      name: 'ember-ecsy-modifier',
+      plugin: EcsyModiferTransform,
+      baseDir() {
+        return __dirname;
+      },
+      cacheKey() {
+        return 'ember-ecsy-modifier';
+      }
+    };
+  },
 };
