ba0964c69adfe3c34225d5ee62309ef7a5d64e0f
Nick Schot <nickschot@gmail.com>
2019-11-04 17:12:41 +0100

    only traverse parent argument

--- addon/components/ecsy/entity/component.ts
+++ addon/components/ecsy/entity/component.ts
@@ -1,18 +1,17 @@
-import Component from '@ember/component';
 import { Entity, World } from 'ecsy';
 import { assert } from '@ember/debug';
 import EntityComponent from 'ember-babylon/ecsy/components/entity';
 import DomlessGlimmerComponent from 'ember-babylon/components/domless-glimmer/component';
 
 // TODO: we use parent for our own ref
-function findParentEntity(component: Component | DomlessGlimmerComponent) {
+function findParentEntity(component: DomlessGlimmerComponent) {
   // @ts-ignore: ignore private API usage
-  let pointer = component.parentView || component.args.parent;
+  let pointer = component.args.parent;
   while (pointer) {
     if (pointer.__ECSY_ENTITY__) {
       return pointer;
     }
-    pointer = pointer.parentView || component.args.parent;
+    pointer = pointer.args.parent;
   }
 }
 
@@ -28,12 +27,9 @@ export default class EcsyEntity extends DomlessGlimmerComponent<EcsyEntityArgs> 
   constructor(owner: unknown, args: EcsyEntityArgs) {
     super(owner, args);
 
-    console.log('entity parent', args.parent);
-
     assert('A `createEntity` function must be passed to `<Ecsy::Entity/>`', !!this.args.createEntity);
 
     const parentEntityComponent = findParentEntity(this);
-    console.log('ancestor entity', parentEntityComponent);
     const parentEntity = parentEntityComponent ? parentEntityComponent.entity : null;
 
     assert('Parent <Entity/> does not have a valid ECSY Entity component.', (!parentEntityComponent || !!parentEntity));
