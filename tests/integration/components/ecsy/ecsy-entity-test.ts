72660cde468c758c5aa2dd340932309e08e3d8fa
simonihmig <simon.ihmig@kaliber5.de>
2021-11-23 18:18:26 +0100

    Fix helper usage in tests

--- tests/integration/components/ecsy/ecsy-entity-test.ts
+++ tests/integration/components/ecsy/ecsy-entity-test.ts
@@ -8,11 +8,15 @@ module('Integration | Component | ecsy/entity', function (hooks) {
   setupRenderingTest(hooks);
   const getData = setupDataDumper(hooks);
 
+  hooks.beforeEach(function () {
+    this.set('systems', []);
+  });
+
   test('it adds entity', async function (assert) {
     this.set('show', false);
     this.set('components', new Map());
     await render(hbs`
-      <Ecsy @systems={{(array)}} @components={{this.components}} as |world|>
+      <Ecsy @systems={{this.systems}} @components={{this.components}} as |world|>
         {{dump world.world}}
         {{#if this.show}}
           <world.Entity />
